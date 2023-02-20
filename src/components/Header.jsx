5427643816e1daa8fa42a186adb5f971b651ff74
Vitalii Drobinskyi <drobinskyi@hotmail.com>
2023-02-20 16:44:36 +0200

    Create a new project

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+const Header = () => {
+    return <header className='header'>
+        <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Twitter_t_Logo.svg/1200px-Twitter_t_Logo.svg.png' alt='logo' />
+    </header>
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,26 @@
+const Profile = () => {
+    return <div className='content'>
+        <div>
+            <img src='https://i0.wp.com/www.flutterbeads.com/wp-content/uploads/2022/01/add-image-in-flutter-hero.png?fit=2850%2C1801&ssl=1' alt='mountain' />
+        </div>
+        <div>
+            ava + description
+        </div>
+        <div>
+            my posts
+            <div>
+                new post
+            </div>
+            <div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+            </div>
+        </div>
+    </div>
+}
+
+export default Profile;
