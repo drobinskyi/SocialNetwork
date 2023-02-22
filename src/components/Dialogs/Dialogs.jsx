8ce2d9106be8bb1a97716fb500c97468bba5bf58
Vitalii Drobinskyi <drobinskyi@hotmail.com>
2023-02-22 18:57:36 +0200

    Layout of Messages

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,30 @@ import s from './Dialogs.module.css';
 const Dialogs = (props) => {
 
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog +' '+ s.active}>
+                    Dmytro
+                </div>
+                <div className={s.dialog}>
+                    Andriy
+                </div>
+                <div className={s.dialog}>
+                    Svitlana
+                </div>
+                <div className={s.dialog}>
+                    Sasha
+                </div>
+                <div className={s.dialog}>
+                    Viktor
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>How is your day?</div>
+                <div className={s.message}>Nice!</div>
+            </div>
+
         </div>
     )
 
