f23fca209edbc98cc30f7ace15cf481df27fa09d
Vitalii Drobinskyi <drobinskyi@hotmail.com>
2023-04-06 23:16:04 +0300

    Created Context API

--- src/components/Dialogs/DialogsContainer.jsx
+++ src/components/Dialogs/DialogsContainer.jsx
@@ -1,23 +1,29 @@
 import React from 'react';
 import { sendMessageCreator, updateNewMessageBodyActionCreator } from '../../redux/dialogs-reducer';
 import Dialogs from './Dialogs';
+import StoreContext from '../../StoreContext';
 
-const DialogsContainer = (props) => {
+const DialogsContainer = () => {
 
-    let state = props.store.getState().dialogsPage;
-
-    let onSendMessageClick = () => {
-        props.store.dispatch(sendMessageCreator());
-    }
-
-    let onNewMessageChange = (body) => {
-        props.store.dispatch(updateNewMessageBodyActionCreator(body));
-    }
-
-    return <Dialogs updateNewMessageBody={onNewMessageChange} sendMessage={onSendMessageClick}
-                    dialogsPage={state} />
     
 
+    return <StoreContext.Consumer>
+        {store => {
+            let state = store.getState().dialogsPage;
+
+            let onSendMessageClick = () => {
+                store.dispatch(sendMessageCreator());
+            }
+        
+            let onNewMessageChange = (body) => {
+                store.dispatch(updateNewMessageBodyActionCreator(body));
+            }
+
+            return <Dialogs updateNewMessageBody={onNewMessageChange} 
+                            sendMessage={onSendMessageClick}
+                            dialogsPage={state} />
+        }}
+    </StoreContext.Consumer> 
 }
 
 export default DialogsContainer;
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import store from './redux/redux-store';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from './StoreContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -12,7 +13,9 @@ let rerenderEntireTree = (state) => {
   root.render(
     <React.StrictMode>
       <BrowserRouter>
-        <App state={state} dispatch={store.dispatch.bind(store)} store={store} />
+        <Provider store={store}>
+          <App />
+        </Provider>
       </BrowserRouter>
     </React.StrictMode>
   );
