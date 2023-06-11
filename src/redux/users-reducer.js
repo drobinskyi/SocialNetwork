5789a69ee009dd72eef79bb0a50452f1147eaf8d
Vitalii Drobinskyi <drobinskyi@hotmail.com>
2023-06-11 23:36:37 +0300

    Created the function getUser

--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -3,11 +3,7 @@ const UNFOLLOW = 'UNFOLLOW';
 const SET_USERS = 'SET_USERS';
 
 let initialState = {
-    users: [
-        // { id: 1, followed: false, photoUrl: 'https://img.freepik.com/premium-vector/happy-smiling-young-man-avatar-3d-portrait-man-cartoon-character-people-vector-illustration_653240-187.jpg', fullName: 'Dmytro', status: 'I am a boss', location: { city: 'Kyiv', country: 'Ukraine' } },
-        // { id: 2, followed: true, photoUrl: 'https://img.freepik.com/premium-vector/happy-smiling-young-man-avatar-3d-portrait-man-cartoon-character-people-vector-illustration_653240-187.jpg', fullName: 'Ann', status: 'Hello', location: { city: 'VInnytsia', country: 'Ukraine' } },
-        // { id: 3, followed: false, photoUrl: 'https://img.freepik.com/premium-vector/happy-smiling-young-man-avatar-3d-portrait-man-cartoon-character-people-vector-illustration_653240-187.jpg', fullName: 'Vitalii', status: 'I am a boss too', location: { city: 'Lviv', country: 'Ukraine' } } 
-    ]  
+    users: []  
 };
 
 const usersReducer = (state = initialState, action) => {
