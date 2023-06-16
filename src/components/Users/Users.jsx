e4db450488139583f9bf1b121117c29b8d8c8188
Vitalii Drobinskyi <drobinskyi@hotmail.com>
2023-06-16 23:02:39 +0300

    Added a user request to componentDidMount

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -5,8 +5,7 @@ import userPhoto from '../../assets/images/user.png';
 
 class Users extends React.Component {
 
-    constructor(props) {
-        super(props)
+    componentDidMount() {
         axios.get('https://social-network.samuraijs.com/api/1.0/users')
             .then(response => {
                 this.props.setUsers(response.data.items);
@@ -15,7 +14,6 @@ class Users extends React.Component {
 
     render() {
         return <div>
-            {/* <button onClick={this.getUsers}>Get Users</button> */}
             {
                 this.props.users.map(u => <div key={u.id}>
                     <span>
